fe05ffc13512e6f428bd5624d64d547172b59fa6
tkroll6 <tkroll6@wgu.edu>
2024-04-26 06:56:50 -0700

    Creates new project

---
+++ src/test/java/com/example/d288/D288ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.d288;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D288ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
