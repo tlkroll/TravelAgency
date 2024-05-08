f3ca73481e591d099e6a5c26e6940f81bc50e2ff
tkroll6 <tkroll6@wgu.edu>
2024-05-08 04:50:52 -0700

    Part H - Creates CheckoutController class in controllers package

---
+++ src/main/java/com/example/d288/controllers/CheckoutController.java
@@ -0,0 +1,26 @@
+package com.example.d288.controllers;
+
+import com.example.d288.dto.Purchase;
+import com.example.d288.dto.PurchaseResponse;
+import com.example.d288.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService){
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/service")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase){
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+}
